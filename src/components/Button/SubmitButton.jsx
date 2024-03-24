a0ba39458467775ac23887d62e1b4591da5968d5
changhyeon <changhyeon.h@kakao.com>
2024-03-24 15:52:45 +0900

    refactor: Button과 TextField 컴포넌트로 분리

---
+++ src/components/Button/SubmitButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+
+export const SubmitButton = (handleButtonSubmit) => {
+    return <CustomButton className="size-button" variant="contained" onClick={handleButtonSubmit}>입력</CustomButton>;
+}
+
+const CustomButton = styled(Button)({
+    backgroundColor: '#3f3bde',
+    height: '55px',
+})
--- src/pages/MainPage/components/Section.jsx
+++ src/pages/MainPage/components/Section.jsx
@@ -4,40 +4,32 @@ import { getGameCharacterSearchRank } from "src/api/games/getGameCharacterSearch
 import { getMapleStoryMCharacterInfo } from "src/api/games/getMapleStoryMCharacterInfo";
 import { SectionContainer, SearchConatiner, SectionHeader, SelectGameContainer } from "./Section.style";
 import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { TextField } from "@mui/material";
-import Button from "@mui/material/Button";
-import styled from "@emotion/styled";
+
 import { ErrorModal } from "src/components/Modal/ErrorModal";
 import { mapleStoryWorldNames } from "src/constants/mapleStory";
 import { gameInfo } from "src/constants/games";
 import { RankingTable } from "src/components/Ranking/Ranking";
+import { SubmitButton } from "src/components/Button/SubmitButton";
+import { CharacterNameTextField } from "src/components/TextField/CharacterNameTextField";
+import { GameDetailSelect } from "src/components/Select/GameDetailSelect";
+import { GameSelect } from "src/components/Select/GameSelect";
 
 export const Section = () => {
-    const mapleStoryWorldNamesComponents = mapleStoryWorldNames.map((value, index) => (<MenuItem key={index} value={value}>{value}</MenuItem>))
-    const gameInfosComponents = gameInfo.map((value, index) => (<MenuItem key={index} value={value.name}>{value.name}</MenuItem>))
-
-    const navigate = useNavigate();
+    const characterInputError = new Error("캐릭터명 또는 월드명을 입력해주세요.");
+    const errorModalTitle = "에러 메시지입니다.";
 
     const [characterName, setCharacterName] = useState('');
     const [characterWorldName, setCharacterWorldName] = useState('');
     const [selectedGame, setSelectedGame] = useState({});
     const [rank, setRank] = useState([]);
     const [timer, setTimer] = useState(10);
-    const [isRankDataAvailable, setIsRankDataAvailable] = useState(false); 
-
+    const [isRankDataAvailable, setIsRankDataAvailable] = useState(false);
     const [errorModalOpen, setErrorModalOpen] = useState(false);
     const [errorModalContent, setErrorModalContent] = useState('');
-    const errorModalTitle = "에러 메시지입니다.";
 
-    const handleChange = (event) => {
-        setCharacterWorldName(event.target.value);
-    };
+    const navigate = useNavigate();
 
+    // 선택한 게임에 따라 변경되는 Input 값을 처리하기 위한 메서드
     const handleChangeGame = (event) => {
         const game = event.target.value;
 
@@ -57,6 +49,7 @@ export const Section = () => {
         setSelectedGame(selected[0]);
     };
 
+    // 랭킹 데이터 갱신
     useEffect(() => {
         const fetchData = async () => {
             const rankData = await getGameCharacterSearchRank(selectedGame.uri);
@@ -84,12 +77,10 @@ export const Section = () => {
     }, [selectedGame]);
 
 
-    const getCharacerData = async () => {
+    // 캐릭터 정보 불러오는 메서드
+    const getCharacterData = async () => {
         if (characterName === '' || characterWorldName === '') {
-            const error = new Error();
-            error.message = "캐릭터명 또는 월드명을 입력해주세요";
-
-            handleErrorModalOpen(error);
+            handleErrorModalOpen(characterInputError);
             return;
         }
 
@@ -105,13 +96,19 @@ export const Section = () => {
         });
     }
 
+    // 월드명 Select 갱신 메서드
+    const handleChange = (event) => {
+        setCharacterWorldName(event.target.value);
+    };
+
+    // ErrorModal 관련 메서드
     const handleErrorModalOpen = (error) => {
         if (error.code === "API-002") {
             setErrorModalContent("없는 캐릭터이거나 잘못된 입력 값일 수도 있습니다. 다시 한 번 시도해주세요.");
         } else {
             setErrorModalContent(error.message);
         }
-        
+
         setErrorModalOpen(true);
     };
 
@@ -127,45 +124,31 @@ export const Section = () => {
                     모바일 게임 캐릭터 조회 서비스
                 </SectionHeader>
                 <SelectGameContainer>
-                    <FormControl variant="standard" sx={{ m: 2, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-standard-label">게임명</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard"
-                            value={selectedGame.name}
-                            onChange={handleChangeGame}
-                            label="게임명"
-                        >
-                            {gameInfosComponents}
-                        </Select>
-                    </FormControl>
+                    <GameSelect
+                        menuData={gameInfo}
+                        selectValue={selectedGame}
+                        handleSelectOnChange={handleChangeGame}
+                    />
                 </SelectGameContainer>
+                <SearchConatiner>
+                    {
+                        selectedGame.name === '메이플스토리M' ?
+                            <>
+                                <GameDetailSelect
+                                    menuData={mapleStoryWorldNames}
+                                    selectValue={characterWorldName}
+                                    handleSelectOnChange={handleChange}
+                                />
+                                <CharacterNameTextField
+                                    setInputValue={setCharacterName}
+                                    handleEnterSubmit={getCharacterData}
+                                />
+                                <SubmitButton handleButtonSubmit={getCharacterData} />
+                            </> : <></>
+                    }
+                </SearchConatiner>
                 {
-                    selectedGame.name === '메이플스토리M' ? <SearchConatiner>
-                        <Box sx={{ minWidth: 120 }} className="form-control">
-                            <FormControl fullWidth>
-                                <InputLabel className="table-cell" id="filled-basic">월드명</InputLabel>
-                                <Select
-                                    className="table-cell"
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={characterWorldName}
-                                    label="월드명"
-                                    onChange={handleChange}
-                                >
-                                    {mapleStoryWorldNamesComponents}
-                                </Select>
-                            </FormControl>
-                        </Box>
-                        <TextField className="table-cell" required id="outlined-required" label="캐릭터명" onChange={(e) => setCharacterName(e.target.value)} onKeyDown={(e) => {
-                            if(e.key === 'Enter'){
-                                getCharacerData();
-                            }
-                        }}/>
-                        <SizeButton className="size-button" variant="contained" onClick={getCharacerData}>입력</SizeButton>
-                    </SearchConatiner> : <></>
-                }{
-                    selectedGame.name && isRankDataAvailable ? <RankingTable rank={rank} timer={timer} /> : <></>
+                    selectedGame.name && isRankDataAvailable && <RankingTable rank={rank} timer={timer} />
                 }
             </SectionContainer>
             <ErrorModal
@@ -179,8 +162,3 @@ export const Section = () => {
     )
 }
 
-
-const SizeButton = styled(Button)({
-    backgroundColor: '#3f3bde',
-    height: '55px',
-})
