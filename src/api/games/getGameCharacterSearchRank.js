c3b75a103399f5003a03a33eb3b9507f2df85bb6
changhyeon <changhyeon.h@kakao.com>
2024-07-23 17:12:09 +0900

    refactor: api 주소 변경 및 일부 에러 수정

--- src/api/games/getGameCharacterSearchRank.js
+++ src/api/games/getGameCharacterSearchRank.js
@@ -1,7 +1,7 @@
 import axiosInstance from "src/api/axiosInstance";
 
 export const getGameCharacterSearchRank = async (gameType) => {
-    const { data } = await axiosInstance.get(`/games/${gameType}/rank`);
+    const { data } = await axiosInstance.get(`/rank/${gameType}`);
 
     return data;    
 }
